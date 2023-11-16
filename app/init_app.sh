0099f72a3d2e83ce6ff58c707ba33f48fd099c2f
Yohan Foucade <yohan.foucade@gmail.com>
2023-11-16 10:41:41 +0100

    configure app container, expose port

--- app/init_app.sh
+++ app/init_app.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
+# use pip freeze > requirements.txt
+# pip install -r requirements.txt
 source /env/bin/activate
 pip install --upgrade pip
 pip install Django==4.2.7
+pip install psycopg
 
 exec "$@"
