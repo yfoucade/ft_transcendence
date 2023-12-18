cb026d7ee4fca75632876e2f0e8ee1abbfd5ce49
Yohan Foucade <yohan.foucade@gmail.com>
2023-12-18 20:54:31 +0100

    add STATIC_ROOT for collectstatic

--- app/src/config/settings.py
+++ app/src/config/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # TODO: see https://docs.djangoproject.com/en/4.2/howto/static-files/deployment/
 # for proper strategies to serve static files in production environments.
 STATIC_URL = 'static/'
+STATIC_ROOT = "/var/www/transcendence/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
