201fea8a01cfc96bd13ff5fbc7cc7bf0c640a63f
Yohan Foucade <yohan.foucade@gmail.com>
2023-12-18 20:54:52 +0100

    deploy using daphne and nginx

--- app/init_app.sh
+++ app/init_app.sh
@@ -4,7 +4,12 @@
 # pip install -r requirements.txt
 source /env/bin/activate
 pip install --upgrade pip
-pip install Django==4.2.7
+# pip install Django==4.2.7
+pip install Django
 pip install psycopg
+pip install daphne
 
+cd src/
+python manage.py collectstatic
+nginx
 exec "$@"
