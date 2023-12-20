ed37495f3fbaaf5a2cfe623d17d895b7dc95db5b
Yohan Foucade <yohan.foucade@gmail.com>
2023-12-20 14:19:32 +0100

    keep secret key secret

--- app/src/config/settings.py
+++ app/src/config/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+# TODO: keep secret key used in production secret!
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-zig57@5d4$kn%i1((e%dzm4xtin#&1%ad^cyjjuf=+bqmnym24'
 
