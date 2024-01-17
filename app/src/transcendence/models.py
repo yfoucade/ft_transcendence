7990c21d574e9f41198cf1421cdf197b0edecbfe
Yohan Foucade <yohan.foucade@gmail.com>
2024-01-17 23:35:48 +0100

    create profile model

--- app/src/transcendence/models.py
+++ app/src/transcendence/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+def get_thumbnail_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/thumbnail
+    return "user_{0}/thumbnail".format(instance.user.id)
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete = models.CASCADE,
+    )
+    display_name = models.CharField(max_length=10, blank=False, unique=True)
+    # thumbnail = models.ImageField(upload_to=get_thumbnail_path, default="default.jpg")
+    following = models.ManyToManyField(User, related_name="followers")
+    rating = models.FloatField(default=1500)
