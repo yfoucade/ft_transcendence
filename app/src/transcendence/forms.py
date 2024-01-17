6dd37f69ae8cfc5d80f9b6ec8693ddfa5dcaabea
Yohan Foucade <yohan.foucade@gmail.com>
2024-01-17 23:34:33 +0100

    feat: enforce email uniqueness

--- app/src/transcendence/forms.py
+++ app/src/transcendence/forms.py
@@ -1,8 +1,19 @@
+from django.core.exceptions import ValidationError
 from django.forms import CharField, Textarea
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email",)
+
+    def clean_email(self):
+        print("Validating email") 
+        data = self.cleaned_data.get("email")
+        if User.objects.filter(email=data).exists():
+            msg = 'A user with that email already exists.'
+            raise ValidationError(msg, code="email_duplicate")
+
+        return data
