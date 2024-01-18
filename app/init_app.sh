1bb9da28cb7f04988bec3c2288766c1faea8f8dc
Yohan Foucade <yohan.foucade@gmail.com>
2024-01-18 19:54:13 +0100

    remove old migrations on each buil

--- app/init_app.sh
+++ app/init_app.sh
@@ -11,6 +11,8 @@ pip install daphne
 pip install Pillow
 
 cd src/
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc"  -delete
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
