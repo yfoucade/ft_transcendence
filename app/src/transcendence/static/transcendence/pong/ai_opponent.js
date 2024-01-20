aa9aa617a5ef34b1acd4282a455f4724f0a0f14a
HaniaKhater <hania.khater2@hotmail.com>
2024-01-20 02:13:18 +0100

    Ai opponent gets ballY every second but just goes all the way up and down

---
+++ app/src/transcendence/static/transcendence/pong/ai_opponent.js
@@ -0,0 +1,39 @@
+function fetchBallYCoordinate() {
+    console.log("Time check");
+    const ballElement = pong_game.html_element_ball;
+    const ballRect = ballElement.getBoundingClientRect();
+    const ballYCoordinate = ballRect.top + ballRect.height / 2;
+
+    simulateKeyboardInput(ballYCoordinate);
+}
+
+function fetchPaddleYCoordinate() {
+    const leftPaddleElement = pong_game.html_element_left_paddle;
+    const leftPaddleRect = leftPaddleElement.getBoundingClientRect();
+    const leftPaddleYCoordinate = leftPaddleRect.top + leftPaddleRect.height / 2;
+
+    return(leftPaddleYCoordinate);
+}
+
+function simulateKeyboardInput(ballYCoordinate) {
+    let leftPaddleYCoordinate = fetchPaddleYCoordinate();
+
+    if (ballYCoordinate < leftPaddleYCoordinate) {
+        handle_keydown({ key: "e" });
+        handle_keyup({ key: "d" });
+    }
+    else if (ballYCoordinate > leftPaddleYCoordinate) {
+        handle_keydown({ key: "d" });
+        handle_keyup({ key: "e" });
+    } else {
+        handle_keyup({ key: "e" });
+        handle_keyup({ key: "d" });
+    }
+}
+
+function launchAiOpponent() {
+    // Use setInterval as an alternative to sleep
+    setInterval(fetchBallYCoordinate, 1000);
+}
+
+launchAiOpponent();
