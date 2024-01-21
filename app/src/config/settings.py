a5fef979efa511dee126738278a1be887bd59544
Yohan Foucade <yohan.foucade@gmail.com>
2024-01-21 05:37:42 +0100

    add middleware to update last_request_time in Profile

--- app/src/config/settings.py
+++ app/src/config/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'transcendence.middleware.UpdateLastRequestTimeMiddleware'
 ]
 
 ROOT_URLCONF = 'config.urls'
