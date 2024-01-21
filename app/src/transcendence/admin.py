6612da6743b9023dcdf39e471f7d7a027041c2b0
Yohan Foucade <yohan.foucade@gmail.com>
2024-01-21 05:38:47 +0100

    update admin's list_diplay for Profile

--- app/src/transcendence/admin.py
+++ app/src/transcendence/admin.py
@@ -4,4 +4,6 @@ from .models import Profile
 
 # Register your models here.
 
-admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["user", "display_name", "rating"]
