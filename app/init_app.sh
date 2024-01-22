6a06fabc7dcd40a833685f2b02a20cd525fc3fd0
Yohan Foucade <yohan.foucade@gmail.com>
2024-01-22 23:20:27 +0100

    build: modify some paths

--- app/init_app.sh
+++ app/init_app.sh
@@ -11,13 +11,13 @@ pip install daphne
 pip install Pillow
 
 cd src/
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/migrations/*.pyc"  -delete
+# find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+# find . -path "*/migrations/*.pyc"  -delete
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py createsuperuser --noinput
-cd /
+# cd /
 
 nginx
 exec "$@"
