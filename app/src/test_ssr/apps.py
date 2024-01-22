164af2e3bb4591bc037087344392bdd0c8a2af2a
Yohan Foucade <yohan.foucade@gmail.com>
2024-01-22 13:04:49 +0100

    remove test_ssr app

--- app/src/test_ssr/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestSsrConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'test_ssr'
--- app/src/test_ssr/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path( "view_a", views.view_a, name="view_a" ),
-    path( "view_b", views.view_b, name="view_b" ),
-]
