3897f2a2851ab2e56140e4948da90908e91bf7aa
Yohan Foucade <yohan.foucade@gmail.com>
2024-01-27 00:56:16 +0100

    start implementing online game

---
+++ app/src/transcendence/pong/online_game/online_game.py
@@ -0,0 +1,111 @@
+import sys
+sys.path.append("../../../")
+
+import asyncio
+import json
+
+from django.db.models import Q
+from django.contrib.auth.models import User
+
+from transcendence.models import PongGame
+
+async def assign_to_game(user:User):
+    already_waiting = await PongGame.objects.filter(
+        Q(game_status__exact="init"),
+        Q(user_1_id=user.id)
+    ).afirst()
+    if already_waiting:
+        return already_waiting
+    initialized_games = await PongGame.objects.filter(game_status="init").afirst()
+    res = initialized_games or PongGame()
+    await res.asave()
+    updated_fields = await res.add_player(user)
+    await res.asave(update_fields=updated_fields)
+    return res
+
+def build_message(msg_dict):
+    res = '\n'.join(f"{item[0]}: {item[1]}" for item in msg_dict.items())
+    return res + "\n\n"
+
+async def send_game_id(game:PongGame):
+    msg_data = {"game_id": f"{game.id}"}
+    msg = {"event":"wait",
+           "data":json.dumps(msg_data)}
+    txt = build_message(msg)
+    yield txt
+
+async def wait_second_player(game:PongGame):
+    while True:
+        await game.arefresh_from_db()
+        if game.user_2_id:
+            return
+
+
+
+async def dummy_fun(n=1):
+    for i in range(10):
+        print(f"{i}: in dummy fun {n}")
+        sys.stdout.flush()
+        await asyncio.sleep(.2)
+
+async def dummy_gen(n=1):
+    for i in range(10):
+        print(f"{i}: in dummy gen {n}")
+        sys.stdout.flush()
+        yield build_message({"event":"debug", "data":f"{i} in dummy {n}"})
+        await asyncio.sleep(.2)
+
+async def game_loop(game:PongGame):
+    """
+    - Initialize a game engine
+    - Set the init_game_str attribute in `game`
+    - wait while timezone.now() < start_time
+    - while status == running, compute next state
+    """
+    pass
+
+async def state_sender(game:PongGame):
+    """
+    - wait while game.init_game_str is not set
+    - yield game.init_game_str
+    - while status == starting, yield seconds remaining
+    - while status == running, yield game state
+    - send 'done' event
+    """
+    pass
+
+async def online_game_stream(request):
+    game = await assign_to_game(request.user)
+    try:
+        async for message in send_game_id(game):
+            yield message
+        await wait_second_player(game)
+        yield build_message({"event":"debug", "data":"found second player"})
+
+        # The following lines are to test concurrent asyncio tasks
+        dummy1 = asyncio.create_task(dummy_fun(1)) if game.user_1_id == request.user.pk else None
+        dummy1
+        async for msg in dummy_gen(2):
+            yield msg
+        # It worked, both threads ran concurrently
+        # The thread in charge of running the game loop will run two tasks:
+        # game_loop + event_sender
+        # The other thread will only yield from the event_sender
+
+        loop_task = asyncio.create_task(game_loop(game)) if game.user_1_id == request.user.pk else None
+        loop_task
+        async for msg in state_sender(game):
+            yield msg
+
+        yield build_message({"event":"close", "data":""})
+    except asyncio.CancelledError:
+        if not game:
+            return
+        elif game.game_status == "init":
+            await game.adelete()
+            return
+        elif game.game_status == "starting":
+            updated_fields = game.set_status("aborted")
+        elif game.game_status == "running":
+            updated_fields = game.abort_running_game(request.user)
+        await game.asave(update_fields=updated_fields)
