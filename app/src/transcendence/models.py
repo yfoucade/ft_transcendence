9e8f4c72a4b4e7d5773baa9e5011ff439cd0efba
Yohan Foucade <yohan.foucade@gmail.com>
2024-01-28 09:36:40 +0100

    feat: set game elements pct values, and make methods async

--- app/src/transcendence/models.py
+++ app/src/transcendence/models.py
@@ -1,3 +1,5 @@
+import json
+
 from datetime import timedelta
 from random import random
 
@@ -38,47 +40,58 @@ class PongGame(models.Model):
     game_status = models.CharField(default="init") # starting, running, aborted, over
     end_reason = models.CharField(null=True) # points, deconnection
     winner = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, related_name="games_won")
-    left_points = models.SmallIntegerField(default=0)
-    right_points = models.SmallIntegerField(default=0)
+    left_score = models.SmallIntegerField(default=0)
+    right_score = models.SmallIntegerField(default=0)
 
     # in-game elements
-    ball_top = models.CharField(null=True)
+    ball_top_pct = models.FloatField(null=True)
+    ball_left_pct = models.FloatField(null=True)
+    left_paddle_top_pct = models.FloatField(null=True)
+    right_paddle_top_pct = models.FloatField(null=True)
 
     @sync_to_async
     def add_player(self, user:User, start_delay:timedelta=timedelta(seconds=3)):
         """
         Adds user `user` to the list of players.
         If there is already one registered player, adds the second player
-        and also sets the status to 'running' and the start time to
-        now + start_delay.
+        and also sets the status to 'starting'.
         """
 
         if not self.user_1:
             self.user_1 = user
             return ["user_1"]
         self.user_2 = user
-        if random() < .5:
-            self.user_1, self.user_2 = self.user_2, self.user_1
-        self.start_time = timezone.now() + start_delay
+        # if random() < .5:
+        #     self.user_1, self.user_2 = self.user_2, self.user_1
         self.game_status = "starting"
-        return ["user_1", "user_2", "game_status", "start_time"]
+        return ["user_1", "user_2", "game_status"]
 
-    def set_status(self, new_status:str):
+    def set_start_time(self, start_delay = timedelta(seconds=3)):
+        self.start_time = timezone.now() + start_delay
+        return ["start_time"]
+
+    async def set_status(self, new_status:str):
         self.game_status = new_status
         return ["game_status"]
-    
-    def set_loser(self, user:User):
-        self.winner = self.user_2 if (self.user_1 == user.pk) else self.user_1
+
+    async def set_loser(self, user:User):
+        if (self.user_1_id == user.pk):
+            self.winner = await User.objects.aget(pk=self.user_2_id)
+        else:
+            self.winner = await User.objects.aget(pk=self.user_1_id)
+        # self.winner = self.user_2 if (self.user_1_id == user.pk) else self.user_1
+        #               ^^^^^^^^^^^
+        #               SynchronousOnlyOperation
         return ["winner"]
 
-    def set_end_reason(self, reason:str):
+    async def set_end_reason(self, reason:str):
         self.end_reason = reason
         return ["end_reason"]
 
-    def abort_running_game(self, user:User):
-        updated_fields = self.set_status("done")
-        updated_fields += self.set_end_reason("deconnection")
-        updated_fields += self.set_loser(user)
+    async def abort_running_game(self, user:User):
+        updated_fields = await self.set_status("done")
+        updated_fields += await self.set_end_reason("deconnection")
+        updated_fields += await self.set_loser(user)
         return updated_fields
 
     @sync_to_async
@@ -91,3 +104,30 @@ class PongGame(models.Model):
     def set_init_game_str(self, str):
         self.init_game_str = str
         return ["init_game_str"]
+
+    def set_game_elements(self, *, lpt=None, rpt=None, bl=None, bt=None):
+        res = []
+        if lpt:
+            self.left_paddle_top_pct = lpt
+            res += ["left_paddle_top_pct"]
+        if rpt:
+            self.right_paddle_top_pct = rpt
+            res += ["right_paddle_top_pct"]
+        if bl:
+            self.ball_left_pct = bl
+            res += ["ball_left_pct"]
+        if bt:
+            self.ball_top_pct = bt
+            res += ["ball_top_pct"]
+        return res
+    
+    def get_position_str(self):
+        res = {
+            "left_score": self.left_score,
+            "right_score": self.right_score,
+            "left_paddle_top_pct": self.left_paddle_top_pct,
+            "right_paddle_top_pct": self.right_paddle_top_pct,
+            "ball_top_pct": self.ball_top_pct,
+            "ball_left_pct": self.ball_left_pct,
+        }
+        return json.dumps(res)
