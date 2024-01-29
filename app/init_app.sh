9658aedbd95f7626e53ff24d8fcdd31f4995119d
Yohan Foucade <yohan.foucade@gmail.com>
2024-01-29 13:05:12 +0100

    install channels, channels_redis, and start redis server

--- app/init_app.sh
+++ app/init_app.sh
@@ -7,8 +7,10 @@ pip install --upgrade pip
 # pip install Django==4.2.7
 pip install Django
 pip install psycopg
+pip install channels
 pip install daphne
 pip install Pillow
+pip install channels_redis
 
 cd src/
 find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
@@ -19,5 +21,6 @@ python manage.py collectstatic --noinput
 python manage.py createsuperuser --noinput
 # cd /
 
+redis-server --daemonize yes
 nginx
 exec "$@"
