63d093a50a33557d851bc9544ae6e185d4e731c7
Yohan Foucade <yohan.foucade@gmail.com>
2024-02-02 00:41:29 +0100

    add online tournament consumer to routes

--- app/src/transcendence/routing.py
+++ app/src/transcendence/routing.py
@@ -5,4 +5,5 @@ from . import consumers
 
 transcendence_websockets = [
     path("ws/online-game/", consumers.OnlineGameConsumer.as_asgi()),
+    path("ws/online-tournament/", consumers.OnlineTournamentConsumer.as_asgi()),
 ]
