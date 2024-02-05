fe02a329bc18dec5405f99d09cd75542e9d09df1
yfoucade <yfoucade@student.42.fr>
2024-02-05 13:04:36 +0100

    build: use requirements.txt

--- app/init_app.sh
+++ app/init_app.sh
@@ -1,16 +1,6 @@
 #! /bin/bash
 
-# use pip freeze > requirements.txt
-# pip install -r requirements.txt
 source /env/bin/activate
-pip install --upgrade pip
-# pip install Django==4.2.7
-pip install Django
-pip install psycopg
-pip install channels
-pip install daphne
-pip install Pillow
-pip install channels_redis
 
 cd src/
 find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
