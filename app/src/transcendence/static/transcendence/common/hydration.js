b328b9202f3ecb2773b448f188b4cdc319b4e62b
yfoucade <yfoucade@student.42.fr>
2024-02-05 13:23:22 +0100

    fix: install handler for links in main-navbar, not just main-menu

--- app/src/transcendence/static/transcendence/common/hydration.js
+++ app/src/transcendence/static/transcendence/common/hydration.js
@@ -1,6 +1,6 @@
 function hydrate_common_elements() {
     console.log("hydrating common elements");
-    let links = document.querySelectorAll("#main-menu a");
+    let links = document.querySelectorAll("#main-navbar a");
     for ( let link of links )
         link.addEventListener( "click", route );
 
