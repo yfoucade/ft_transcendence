d2b8e0220bbab9c9ca7c9b3828fc030f58bac2b5
yfoucade <yfoucade@student.42.fr>
2024-02-11 20:27:55 +0100

    call get_asgi_application before importing routing

--- app/src/config/asgi.py
+++ app/src/config/asgi.py
@@ -14,11 +14,10 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from transcendence.routing import transcendence_websockets
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
 django_asgi_app = get_asgi_application()
+
+from transcendence.routing import transcendence_websockets
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
